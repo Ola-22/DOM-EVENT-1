093224471c88a789ce0e2b54426df901f60dd8b8
Ola-22 <70202013+Ola-22@users.noreply.github.com>
2020-09-30 14:48:14 +0300

    Add files via upload

---
+++ dom_events_01/main.js
@@ -0,0 +1,17 @@
+function tabContent(evt, ids) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(ids).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
